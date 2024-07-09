688083cb34417f1edda7d2c92227e6305cf46dbf
Md. Saminul Amin <saminul.amin@gmail.com>
2024-07-09 15:03:36 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* <h2>Time to get started!</h2> */}
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </div>
+  );
+}
+
+export default App;
